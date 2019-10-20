838a93532322a51b0ea52d545a9975f6739e1b79
Yasmine <yasmine.bouslimani@gmail.com>
2019-10-20 18:35:34 +0200

    quest valided

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once ('../vendor/autoload.php');
+
+use App\Wcs\Hello;
+
+$hello = new Hello();
+
+echo $hello->talk();
+var_dump($hello);
+
+
+
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Wcs;
+
+/**
+ * Class Hello
+ * @package App\Wcs
+ */
+class Hello
+{
+
+    /**
+     * @return string
+     */
+    public function talk(): string
+    {
+
+        return 'Hello World !';
+    }
+}
